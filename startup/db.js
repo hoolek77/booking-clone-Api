9a1b1ef7cd9a11818b68ad25b9da5b54edc0864e
Piotr <p.dybowski13@gmail.com>
2021-02-17 11:34:42 +0100

    changed db name

--- startup/db.js
+++ startup/db.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 
 module.exports = function () {
   mongoose
-    .connect("mongodb://localhost/ExampleDbName", { useNewUrlParser: true })
+    .connect("mongodb://localhost/bookingcloneDB", { useNewUrlParser: true })
     .then(() => console.info("Connected to MongoDB..."));
 };
