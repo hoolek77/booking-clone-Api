493eb20d752d40937d66b9ad063f9fcb13604ba4
Bartosz Białecki <bartosz.bialecki@gmail.com>
2021-02-18 14:44:51 +0100

    Some setup changes

--- startup/prod.js
+++ startup/prod.js
@@ -1,7 +1,15 @@
 const helmet = require('helmet')
 const compression = require('compression')
+const cors = require('cors')
+const mongoSanitize = require('express-mongo-sanitize')
+const xssClean = require('xss-clean')
+const hpp = require('hpp')
 
 module.exports = function (app) {
+  app.use(cors())
   app.use(helmet())
   app.use(compression())
+  app.use(mongoSanitize())
+  app.use(xssClean())
+  app.use(hpp())
 }
--- startup/routes.js
+++ startup/routes.js
@@ -1,8 +1,26 @@
 const express = require('express')
+const rateLimit = require('express-rate-limit')
+
+const ApiError = require('../helpers/apiError')
+const globalErrorHandler = require('../middleware/globalErrorHandler')
+
 const example = require('../routes/example')
 
 module.exports = function (app) {
   app.use(express.json())
 
-  app.use('/api/users', example)
+  const limit = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too Many Requests',
+  })
+
+  app.use('/api', limit)
+  app.use('/api/v1/users', example)
+
+  app.use('*', (req, res, next) => {
+    next(new ApiError(404, 'Route is not supported.'), req, res, next)
+  })
+
+  app.use(globalErrorHandler)
 }
