5a6c034fc19974899e5826ba6a82179b7c030297
hoolek77 <szymon.kin@gmail.com>
2021-02-18 12:16:40 +0100

    created reservation schema

---
+++ schemas/reservation.js
@@ -0,0 +1,69 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const reservationSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
+  hotelId: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
+  roomlId: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+    required: true,
+  },
+  people: {
+    type: new mongoose.Schema({
+      adults: {
+        type: Number,
+        required: true,
+        min: 1,
+      },
+      children: {
+        type: Number,
+        required: true,
+        min: 0,
+      },
+    }),
+    required: true,
+  },
+  isPaid: {
+    type: Boolean,
+    default: false,
+  },
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  },
+});
+
+const Reservation = mongoose.model('Reservation', reservationSchema);
+
+const validateReservation = (reservation) => {
+  const schema = Joi.object({
+    userId: Joi.ObjectId().required(),
+    hotelId: Joi.ObjectId().required(),
+    roomId: Joi.ObjectId().required(),
+    startDate: Joi.date().required(),
+    endDate: Joi.date().required(),
+    people: Joi.object({
+      children: Joi.number.min(0).required(),
+      adults: Joi.number.min(0).required(),
+    }),
+  });
+
+  return schema.validate(reservation);
+};
+
+exports.validate = validateReservation;
+exports.Reservation = Reservation;
