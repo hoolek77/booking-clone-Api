269641b1e0ce45eb2c3c5749af7ee61bb7f12ddc
hoolek77 <szymon.kin@gmail.com>
2021-02-19 23:16:34 +0100

    timestamp change

--- schemas/reservationSchema.js
+++ schemas/reservationSchema.js
@@ -1,51 +1,53 @@
 const Joi = require('joi')
 const mongoose = require('mongoose')
 
-const reservationSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  hotelId: {
-    type: mongoose.Types.ObjectId,
-    ref: 'Hotel',
-    required: true,
-  },
-  roomlId: {
-    type: mongoose.Types.ObjectId,
-    ref: 'Room',
-    required: true,
-  },
-  startDate: {
-    type: Date,
-    required: true,
-  },
-  endDate: {
-    type: Date,
-    required: true,
-  },
-  people: {
-    type: {
-      adults: {
-        type: Number,
-        required: true,
-        min: 1,
-      },
-      children: {
-        type: Number,
-        required: true,
-        min: 0,
-      },
+const reservationSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: true,
     },
-    required: true,
+    hotelId: {
+      type: mongoose.Types.ObjectId,
+      ref: 'Hotel',
+      required: true,
+    },
+    roomlId: {
+      type: mongoose.Types.ObjectId,
+      ref: 'Room',
+      required: true,
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+    },
+    people: {
+      type: {
+        adults: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+        children: {
+          type: Number,
+          required: true,
+          min: 0,
+        },
+      },
+      required: true,
+    },
+    isPaid: {
+      type: Boolean,
+      default: false,
+    },
   },
-  isPaid: {
-    type: Boolean,
-    default: false,
-  },
-  timestamps: true,
-})
+  { timestamps: true }
+)
 
 const Reservation = mongoose.model('Reservation', reservationSchema)
 
