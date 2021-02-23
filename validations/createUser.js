6711c5766fa98b66c29c8a79beb0ba68cf5be787
Bartosz Białecki <bartosz.bialecki@gmail.com>
2021-02-23 15:47:05 +0100

    Add labels to validation rules

--- validations/createUser.js
+++ validations/createUser.js
@@ -5,14 +5,19 @@ const JoiPhoneNumer = Joi.extend(require('joi-phone-number'))
 
 module.exports = function validateCreateUser(req, res, next) {
   const schema = Joi.object({
-    email: Joi.string().max(255).email().required(),
-    password: Joi.string().min(8).max(30).required(),
-    repeatPassword: Joi.valid(Joi.ref('password')),
-    firstName: Joi.string().min(3).max(50).required(),
-    lastName: Joi.string().min(3).max(50).required(),
-    phoneNumber: JoiPhoneNumer.string().max(20).phoneNumber(),
-    tin: Joi.number(),
-    role: Joi.string().valid(USER_ROLE, HOTEL_OWNER_ROLE),
+    email: Joi.string().max(255).email().required().label('Email'),
+    password: Joi.string().min(8).max(30).required().label('Password'),
+    repeatPassword: Joi.valid(Joi.ref('password')).label(
+      'Confirmation password'
+    ),
+    firstName: Joi.string().min(3).max(50).required().label('First name'),
+    lastName: Joi.string().min(3).max(50).required().label('Last name'),
+    phoneNumber: JoiPhoneNumer.string()
+      .max(20)
+      .phoneNumber()
+      .label('Phone number'),
+    tin: Joi.number().label('Tax identification number'),
+    role: Joi.string().valid(USER_ROLE, HOTEL_OWNER_ROLE).label('User role'),
   }).with('password', 'repeatPassword')
 
   validateRequest(req, next, schema)
--- validations/loginUser.js
+++ validations/loginUser.js
@@ -3,8 +3,8 @@ const { validateRequest } = require('../helpers/validationError')
 
 module.exports = function validateLoginUser(req, res, next) {
   const schema = Joi.object({
-    email: Joi.string().max(255).email().required(),
-    password: Joi.string().min(8).max(30).required(),
+    email: Joi.string().max(255).email().required().label('Email'),
+    password: Joi.string().min(8).max(30).required().label('Password'),
   })
 
   validateRequest(req, next, schema)
