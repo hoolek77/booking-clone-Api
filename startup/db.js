600ddb13a9af2cd8f8f72159dbb33a541b10e759
Piotr Dybowski <38506213+pdybowski@users.noreply.github.com>
2021-02-27 23:29:07 +0100

    Deploy 1.0.1
    * Closes #27
    
    * Closes #27
    
    * package json changes for deploy
    
    * setting up db connection for deployment
    
    Co-authored-by: Szymon Kin <68154191+hoolek77@users.noreply.github.com>

--- startup/db.js
+++ startup/db.js
@@ -4,9 +4,11 @@ const config = require('config')
 const host = config.get('database.host')
 const dbName = config.get('database.dbName')
 
+const dbConnectionStringProd = config.get('database.dbConnectionStringProd')
+
 module.exports = function () {
   mongoose
-    .connect(`mongodb://${host}/${dbName}`, {
+    .connect(dbConnectionStringProd || `mongodb://${host}/${dbName}`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
